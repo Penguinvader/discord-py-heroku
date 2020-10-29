8d85b482de5c2af33bb748bc6a31385b35f260d0
Balázs Bartha <balifej@gmail.com>
2020-10-29 04:45:46 +0100

    added all intents to bot

--- bot/main.py
+++ bot/main.py
@@ -1,8 +1,9 @@
 import os
 import random
+import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 TOKEN = os.getenv("DISCORD_TOKEN")
 import random
 
